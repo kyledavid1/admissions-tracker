b8d7e763b1123d4b7f8e2d000958e3a2da3f611a
Chris Mahr <mahrtian@gmail.com>
2016-02-11 17:37:08 -0500

    rename instructor questionnaires controller file

---
+++ academik_portal/app/controllers/instructor_questionnaires_controller.rb
@@ -0,0 +1,23 @@
+class InstructorQuestionnairesController < ApplicationController
+
+
+	def index
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def update
+	end
+
+	def edit
+	end
+
+end
+
