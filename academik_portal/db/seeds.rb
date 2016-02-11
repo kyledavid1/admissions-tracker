4d9a524a428a3717b890e281dd111d2f40b80b1e
Chris Mahr <mahrtian@gmail.com>
2016-02-11 17:15:51 -0500

    fix pluralization naming errors and add seeds file

--- academik_portal/db/seeds.rb
+++ academik_portal/db/seeds.rb
@@ -5,4 +5,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Student.create(email: 'test@test.com', password: 'test')
+30.times do |student|
+  Student.create (
+    name: Faker::Name.name,
+    email: Faker::Internet.free_email,
+    password_digest: Faker::Lorem.word,
+    course: ['WDI', 'UXDI', 'PMI'].sample,    
+    application_essay: Faker::Lorem.paragraph,
+    application_status: ['Phone Interview Pending', 'In-Person Interview', 'Interviews Completed', 'Accepted', "Rejected"].sample
+  )
+end
+
+15.times do |instructor|
+  Instructor.create (
+    name: Faker::Name.name,
+    email: Faker::Internet.safe_email,
+    password_digest: Faker::Lorem.word,
+    course: ['WDI', 'UXDI', 'PMI'].sample    
+  )
+end
+
+15.times do |ao_officer|
+  AdmissionOfficer.create (
+    name: Faker::Name.name,
+    email: Faker::Internet.safe_email,
+    password_digest: Faker::Lorem.word
+  )
+end	
+
+15.times do |iq|
+  InstructorsQuestionnaire.create(
+		strengths: Faker::Hipster.sentence(8),
+		weaknesses: Faker::Hipster.sentence(5),
+		good_fit: [true, false].sample,
+		comments: Faker::Hipster.sentence(15)
+  )
+end
+
+15.times do |ao_ques|
+  AoQuestionnaire.create(
+    course_goals: Faker::Hipster.sentence(10),
+	comments: Faker::Hipster.sentence(10),
+	reason_for_applying: Faker::Hipster.sentence(10)
+  )
+end
