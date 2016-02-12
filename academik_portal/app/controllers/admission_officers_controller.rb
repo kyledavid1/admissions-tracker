360067678cab93d591ecccb7e67269999f39649d
Kyle Stengline <kyle.stengline@gmail.com>
2016-02-12 14:47:19 -0500

    attempts login forms

--- academik_portal/app/controllers/admission_officers_controller.rb
+++ academik_portal/app/controllers/admission_officers_controller.rb
@@ -1,29 +1,30 @@
 class AdmissionOfficersController < ApplicationController
 
+
+	def index 
+		@admin_officers = AdmissionOfficer.all
+	end
+
 	def login
 		admin_user = AdmissionOfficer.find_by(email: params['email'])
 #getting error of invalid hash somewhere within password_digest. Check and re-check names to make sure everything matches.
 		if admin_user && admin_user.authenticate(params['password_digest'])
-			session[:user_name] = admin_user.email
-			@admin = session[:user_name]
+			session[:admin_name] = admin_user.email
+			@admin = session[:admin_name]
 
-			cookies[:username] = admin_user.email
+			cookies[:adminname] = admin_user.email
 			cookies[:age_example] = {:value => "Expires in 10 seconds", :expires => Time.now + 10}
 
-			redirect_to :admission_officers
+			redirect_to admission_officer_path(admin_user)
 		else
 			@error = true
 			render :login_form
 		end
 	end
 
-	def index 
-		@students = Student.all
-	end
-
 	def show
 		# @admin = AdmissionOfficer.find(params[:id])
-		@student = Student.find(params[:id])
+		@admin_officer = AdmissionOfficer.find(params[:id])
 	end
 
 	def new
@@ -33,16 +34,16 @@ class AdmissionOfficersController < ApplicationController
 	end
 
 	def edit
-		@student = Student.find(params[:id])
-		if @student.save
-			redirect_to student_path(student.id)
+		@admin_officer = AdmissionOfficer.find(params[:id])
+		if @admin_officer.save
+			redirect_to admission_officer_path(admin_officer.id)
 		else
 			render :edit
 		end
 	end
 
 	def update
-		@student = Student.find(params[:id])
+		@admin_officer = AdmissionOfficer.find(params[:id])
 	end
 
 end
