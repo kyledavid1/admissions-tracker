e22ec57c1b344a2a62ea3b15957a33466c53ae9c
Kyle Stengline <kyle.stengline@gmail.com>
2016-02-15 16:45:50 -0500

    adds functional timer

--- academik_portal/app/assets/javascripts/timer.js
+++ academik_portal/app/assets/javascripts/timer.js
@@ -2,11 +2,16 @@
 $(document).ready(function(){
 	console.log("loaded");
 
+	// var minutes = 0;
+	// var seconds = 0;
+	var timerId;
+	var end = $(".end");
+	var timer = $("#timer");
 
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
 
-    setInterval(function () {
+   timerId = setInterval(function() {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -21,11 +26,21 @@ function startTimer(duration, display) {
     }, 1000);
 }	
 
+function endTimer() {
+ 	timer.text("Phone Screen Timer");
+  clearInterval(timerId);
+   	// localStorage.removeItem('minutes', 'seconds');
+  };
+
 window.onload = function () {
     var tenMinutes = 60 * 10,
         display = document.querySelector('#timer');
     $(".start").click(function() {
-	startTimer(tenMinutes, display)
-	});
+	startTimer(tenMinutes, display);
+  
+  });
+    end.on('click', function() {
+			endTimer();
+  });
 };
 });
