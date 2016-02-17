49be7a79254c4c18fbb76783d6afa05469630d91
Chris Mahr <mahrtian@gmail.com>
2016-02-16 19:59:21 -0500

    make edits to merge conflict-caused errors

--- academik_portal/app/controllers/application_controller.rb
+++ academik_portal/app/controllers/application_controller.rb
@@ -3,16 +3,15 @@ class ApplicationController < ActionController::Base
 
 	layout 'layout', :except => :index
 
-	class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+		protect_from_forgery with: :exception
 
 	def current_user
 		case session[:user_type]
-		when 'Student' then Student.find_by(id: session[:user_id])
-		when 'Instructor' then Instructor.find_by(id: session[:user_id])
-		when 'AdmissionOfficer' then AdmissionOfficer.find_by(id: session[:user_id])
+			when 'Student' then Student.find_by(id: session[:user_id])
+			when 'Instructor' then Instructor.find_by(id: session[:user_id])
+			when 'AdmissionOfficer' then AdmissionOfficer.find_by(id: session[:user_id])
 		end
 		#need code here to indicate that the person currently logged in is a student
 	end	
