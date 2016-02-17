32dc77f36f7fa527810138a5a2acc32522acc764
Kyle Stengline <kyle.stengline@gmail.com>
2016-02-17 10:20:31 -0500

    saves changes to ao questionnaire

--- academik_portal/app/controllers/admission_officers_controller.rb
+++ academik_portal/app/controllers/admission_officers_controller.rb
@@ -28,18 +28,13 @@ class AdmissionOfficersController < ApplicationController
 	end
 
 	def show
-		# I also want to view all the students by clicking on a link.
-		# I want to display students that belong to their particular admin officer. Reference student to admission_officer_id? Admission officer id is currently nil. We need to assign that a value before we can associate that ao with students.
+		
 		@admin_officer = AdmissionOfficer.find(params[:id])
 		@students = Student.where(application_status: 'Phone Interview Pending')
 		flash[:success] = "Login Succesful!"
-		#display the students that belong to the admin officer.)
-		# @students = Student.all
-		# if @student.id == @admin_officer
-		# 	@students
-		# else
-		# 	render :admission_officer_path
-		# end
+		@students_scheduled = Student.where(application_status: 'Phone Interview Scheduled', admission_officer_id: params[:id])
+		@students_interviewed = Student.where(application_status: 'In-Person Interview Pending', admission_officer_id: params[:id])
+		
 	end
 
 	def new
