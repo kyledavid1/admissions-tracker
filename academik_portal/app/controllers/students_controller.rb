15fe59e7c58c2ef16e1c4f1fda28e2dcbccd09a9
Kyle Stengline <kyle.stengline@gmail.com>
2016-02-17 09:52:27 -0500

    adds schedule updates to students for ao

--- academik_portal/app/controllers/students_controller.rb
+++ academik_portal/app/controllers/students_controller.rb
@@ -64,25 +64,27 @@ class StudentsController < ApplicationController
 
 	def update
 		@id = params[:id]
-  		@student = Student.find(@id)
-        if session[:user_type] == 'Student'
-          	@student.update(student_params)
-          	respond_to do |format|
-			if @student.save
-				StudentMailer.edit_email(@student).deliver_now
-				format.html { redirect_to '/students/login_form', notice: 'You successfully edited your profile.'}
-				format.json { render :show, status: :created, location: @student }
-			else
-				format.html { render :new}
-				format.json { render json: @user.errors, status: :unprocessable_entity }
-			end
-        elsif session[:user_type] == 'Admission Officer'
-         	@student.update_attributes(application_status: "Phone Interview Scheduled", admission_officer_id: session[:user_id])
-        else session[:user_type] == 'Instructor'
-       		@student.update_attributes(application_status: "In-Person Interview Scheduled", instructor_id: session[:user_id])
+  	@student = Student.find(@id)
+      if session[:user_type] == 'Student'
+        @student.update(student_params)
+         respond_to do |format|
+				   if @student.save
+				     StudentMailer.edit_email(@student).deliver_now
+				     format.html { redirect_to '/students/login_form', notice: 'You successfully edited your profile.'}
+				     format.json { render :show, status: :created, location: @student }
+					 else
+				     format.html { render :new}
+				     format.json { render json: @user.errors, status: :unprocessable_entity }
+					 end
+				 end	 
+      elsif session[:user_type] == 'AdmissionOfficer'
+        @student.update_attributes(application_status: "Phone Interview Scheduled", admission_officer_id: session[:user_id])
+        redirect_to admission_officer_path(session[:user_id])
+      else session[:user_type] == 'Instructor'
+        @student.update_attributes(application_status: "In-Person Interview Scheduled", instructor_id: session[:user_id])
        		redirect_to instructor_path(session[:user_id])
-        end
-    end
+      end
+  end
 
     def destroy
     	@student = Student.find(params[:id])
